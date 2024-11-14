da4af68ccaa9297c21ae4639dc4ad205a802604c
ZS-06 <k224510@nu.edu.pk>
2024-11-15 02:00:22 +0500

    transcript created

---
+++ student_transcript.js
@@ -0,0 +1,6 @@
+// Scroll to the top of the page when the "Scroll to Top" button is clicked
+
+// Scroll to the student portal page (this function will need to be defined based on your page logic)
+function goToStudentPortal() {
+    window.location.href = "student.html";  // Modify the path based on your portal page
+}
